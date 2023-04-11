b5e0e14e230f8b2ddfeea4264f29f06d78255c60
Beatriz Aimi <biagioni.aimi@gmail.com>
2023-04-11 11:50:12 -0300

    Add files via upload

---
+++ cachorro.java
@@ -0,0 +1,12 @@
+package dogss;
+
+
+public class cachorro {
+	
+ int tamanho;
+ String raça;
+ 
+ void latir() {
+	 System.out.println("AU, AU, AU, AU !!!");
+ }
+}
---
+++ cachorroTeste.java
@@ -0,0 +1,13 @@
+package dogss;
+public class cachorroTeste {
+
+	public static void main(String[] args) {
+		
+		cachorro beagle = new cachorro();
+		beagle.raça = "Bleagle";
+		beagle.tamanho = 40;
+		beagle.latir();
+
+	}
+
+}
